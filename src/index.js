5c460fb3e96879644deee62b02af35cdb7e22d49
Serena Di Girolamo <serena.digirolamo@gmail.com>
2026-04-27 10:09:04 +0200

    js(feat): add weather search

--- src/index.js
+++ src/index.js
@@ -14,8 +14,7 @@ date1.innerHTML = `${days[now.getDay()]} ${now.getHours()}:${now.getMinutes()}`;
 function submitCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector(".search-input");
-  let city = document.querySelector(".city");
-  city.innerHTML = `${cityInput.value}`;
+
   let cityName = cityInput.value;
   searchCity(cityName);
 }
@@ -24,14 +23,20 @@ function searchCity(cityName) {
   console.log(cityName);
   let apiKey = "a0aca8t7806acb2cb338fb20fbc444o1";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}`;
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(showWeather);
 }
 
-function displayTemperature(response) {
+function showWeather(response) {
   console.log(Math.round(response.data.temperature.current));
   let temperature = document.querySelector(".temperatureValue");
+  let weatherDesc = document.querySelector(".weather-desc");
+  let city = document.querySelector(".city");
   temperature.innerHTML = `${Math.round(response.data.temperature.current)}`;
+  weatherDesc.innerHTML = response.data.condition.description;
+  city.innerHTML = `${response.data.city}`;
 }
 
 let form = document.querySelector(".search-form");
 form.addEventListener("submit", submitCity);
+
+searchCity("Lisbon");
